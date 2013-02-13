6f1b03ed7c4af3fa55f72fea47ba53b843baf056
Davide D'Alto <davide@hibernate.org>
2013-02-13 16:41:55 +0000

    OGM-182 Refactor test for CompositeId

--- hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/NewsID.java
+++ hibernate-ogm-core/src/test/java/org/hibernate/ogm/test/id/NewsID.java
@@ -29,7 +29,8 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class NewsID implements Serializable {
 
-	public NewsID() { super(); }
+	public NewsID() {
+	}
 
 	public NewsID(String title, String author) {
 		this.title = title;
@@ -39,11 +40,21 @@ public class NewsID implements Serializable {
 	private String title;
 	private String author;
 
-	public String getTitle() { return title; }
-	public void setTitle(String title) { this.title = title; }
+	public String getTitle() {
+		return title;
+	}
 
-	public String getAuthor() {	return author; }
-	public void setAuthor(String author) { this.author = author; }
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
 	@Override
 	public boolean equals(Object o) {
